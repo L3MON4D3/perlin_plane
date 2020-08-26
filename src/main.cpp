fa72d4287911395b801ccf28f72593b816caec14
L3MON4D3 <Simljk@outlook.de>
2020-08-27 00:00:38 +0200

    Add shaders, vertices.

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,66 @@
 #define GLFW_EXPOSE_NATIVE_X11
 #include <GLFW/glfw3native.h>
 
+struct PosColorVertex {
+    float x, y, z;
+    uint32_t rgba;
+
+    static void init() {
+        layout
+            .begin()
+            .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float      )
+            .add(bgfx::Attrib::Color0,   4, bgfx::AttribType::Uint8, true)
+            .end();
+    }
+
+    static bgfx::VertexLayout layout;
+};
+
+bgfx::VertexLayout PosColorVertex::layout;
+
+static PosColorVertex cubeVertices[] =
+{
+	{-1.0f,  1.0f,  1.0f, 0xff000000 },
+	{ 1.0f,  1.0f,  1.0f, 0xff0000ff },
+	{-1.0f, -1.0f,  1.0f, 0xff00ff00 },
+	{ 1.0f, -1.0f,  1.0f, 0xff00ffff },
+	{-1.0f,  1.0f, -1.0f, 0xffff0000 },
+	{ 1.0f,  1.0f, -1.0f, 0xffff00ff },
+	{-1.0f, -1.0f, -1.0f, 0xffffff00 },
+	{ 1.0f, -1.0f, -1.0f, 0xffffffff },
+};
+
+static const uint16_t cubeTriList[] =
+{
+	0, 1, 2, // 0
+	1, 3, 2,
+	4, 6, 5, // 2
+	5, 6, 7,
+	0, 2, 4, // 4
+	4, 2, 6,
+	1, 5, 3, // 6
+	5, 7, 3,
+	0, 4, 1, // 8
+	4, 5, 1,
+	2, 3, 6, // 10
+	6, 3, 7,
+};
+
 static void glfw_errorCallback(int error, const char *description)
 {
 	fprintf(stderr, "GLFW error %d: %s\n", error, description);
 }
 
 /**
- * Create new GLFW-Window with dims. GLFW needs to be initialized.
+ * Create new GLFW-Window with dims width x height. GLFW needs to be initialized.
  * @param init Pass empty bgfx::Init.
  */
-GLFWwindow* create_window(int width, int height, bgfx::Init &init) {
+GLFWwindow* create_window(int width, int height) {
+    //Magic code from entry_glfw.cpp from bgfx.
     GLFWwindow *window =
         glfwCreateWindow(width, height, "helloworld", nullptr, nullptr);
+
+    bgfx::Init init;
 
     init.platformData.ndt = glfwGetX11Display();
     init.platformData.nwh = (void*) glfwGetX11Window(window);
@@ -29,46 +77,53 @@ GLFWwindow* create_window(int width, int height, bgfx::Init &init) {
     init.resolution.width = width;
     init.resolution.reset = BGFX_RESET_VSYNC;
 
+    bgfx::init(init);
+
     return window;
 }
 
 int main(int argc, char** argv){
+    //using so lines dont get too long.
+    using namespace bgfx;
+
     RandomGenerator::Perlin pln = RandomGenerator::Perlin(20);
-    //Call renderFrame before bgfx::init (in create_window) to render on this thread.
-    bgfx::renderFrame();
+    //Call renderFrame before init (in create_window) to render on this thread.
+    renderFrame();
     glfwSetErrorCallback(glfw_errorCallback);
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
     //init should not go out of scope until program finishes.
-    bgfx::Init init;
     int width = 100, height = 100;
-    GLFWwindow *window = create_window(width, height, init);
-    bgfx::init(init);
-    if (!window) {
-        std::cout << "lol";
-        return 1;
-    }
-    
+    GLFWwindow *window = create_window(width, height);
 
-    const bgfx::ViewId clearView = 0;
-    bgfx::setViewClear(clearView, BGFX_CLEAR_COLOR);
-    bgfx::setViewRect(clearView, 0, 0, bgfx::BackbufferRatio::Equal);
+    const ViewId clearView = 0;
+    setViewClear(clearView, BGFX_CLEAR_COLOR);
+    setViewRect(clearView, 0, 0, BackbufferRatio::Equal);
+
+    VertexBufferHandle vbh = createVertexBuffer(
+            makeRef(cubeVertices, sizeof(cubeVertices)),
+            PosColorVertex::layout);
+
+    IndexBufferHandle ibh = createIndexBuffer(
+            makeRef(cubeTriList, sizeof(cubeTriList)));
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
         int oldWidth = width, oldHeight = height;
         glfwGetWindowSize(window, &width, &height);
         if (width != oldWidth || height != oldHeight) {
-            bgfx::reset(width, height, BGFX_RESET_VSYNC);
-            bgfx::setViewRect(clearView, 0, 0, bgfx::BackbufferRatio::Equal);
+            reset(width, height, BGFX_RESET_VSYNC);
+            setViewRect(clearView, 0, 0, BackbufferRatio::Equal);
         }
-        
-        bgfx::touch(clearView);
-        bgfx::frame();
+
+        touch(clearView);
+        frame();
     }
 
-    bgfx::shutdown();
+    destroy(vbh);
+    destroy(ibh);
+    shutdown();
     glfwTerminate();
     return 0;
 }
