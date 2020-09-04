042d2fe73771783fca13667d1a6f94c9a8c49d56
L3MON4D3 <Simljk@outlook.de>
2020-09-04 16:36:52 +0200

    Split function.

--- src/Util.cpp
+++ src/Util.cpp
@@ -31,6 +31,11 @@ void glfw_errorCallback(int error, const char *description) {
 	fprintf(stderr, "GLFW error %d: %s\n", error, description);
 }
 
+bx::Vec3 triangle_normal(bx::Vec3 t, bx::Vec3 a, bx::Vec3 b) {
+	return bx::normalize(bx::cross(
+		bx::sub(a, t), bx::sub(b, t) ));
+}
+
 //adds normal-component to Vertex vert_target.
 void add_normal(worldWp::PosNormalColorVertex *vert_target,
     float const *vert_a, float const *vert_b) {
@@ -40,14 +45,13 @@ void add_normal(worldWp::PosNormalColorVertex *vert_target,
     const bx::Vec3 vc_a {vert_a[0], vert_a[1], vert_a[2]};
     const bx::Vec3 vc_b {vert_b[0], vert_b[1], vert_b[2]};
 
-    bx::Vec3 vc_norm {bx::normalize(bx::cross(
-	bx::sub(vc_a, vc_target),
-	bx::sub(vc_b, vc_target) )) };
+    bx::Vec3 vc_norm { triangle_normal(vc_target, vc_a, vc_b) };
 
     //account for offset.
     fl_target[3] = vc_norm.x;
     fl_target[4] = vc_norm.y;
     fl_target[5] = vc_norm.z;
 }
+
 };
 };
--- src/Util.hpp
+++ src/Util.hpp
@@ -1,5 +1,6 @@
 #include "ModelBuilder.hpp"
 #include "bgfx/bgfx.h"
+#include "bx/math.h"
 
 #ifndef UTIL_H_
 #define UTIL_H_
@@ -10,6 +11,7 @@ namespace util {
 bgfx::ShaderHandle load_shader(const char *name);
 void glfw_errorCallback(int error, const char *description);
 void add_normal(worldWp::PosNormalColorVertex *vert_vec, const float* vec_a, const float* vec_b);
+bx::Vec3 triangle_normal(bx::Vec3 t, bx::Vec3 a, bx::Vec3 b);
 
 };
 };
