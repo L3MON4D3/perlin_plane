07d0ab3247fc0c30bd99cde93175c83cb8084170
L3MON4D3 <Simljk@outlook.de>
2020-10-15 12:47:37 +0200

    Move varying.defs around, integrate shader in cmake.

--- src/main.cpp
+++ src/main.cpp
@@ -100,8 +100,8 @@ int main(int argc, char** argv) {
 	VertexBufferHandle frame_vbh{frame.getVBufferHandle()};
 	IndexBufferHandle frame_ibh{frame.getIBufferHandle()};
 
-    ShaderHandle vsh = worldWp::util::load_shader("build/src/vs_simple.bin");
-    ShaderHandle fsh = worldWp::util::load_shader("build/src/fs_simple.bin");
+    ShaderHandle vsh = worldWp::util::load_shader("build/shaders/vs_simple.bin");
+    ShaderHandle fsh = worldWp::util::load_shader("build/shaders/fs_simple.bin");
     ProgramHandle program_simple = createProgram(vsh, fsh, true);
 
     vsh = worldWp::util::load_shader("build/src/vs_height.bin");
